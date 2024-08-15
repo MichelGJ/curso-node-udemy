f4fcbe36d247b4dd7fd6a74c34e46bd07c4e2192
michelgj <michjraich12@hotmail.com>
2024-08-14 21:50:56 -0400

    seccion 21

---
+++ 08-USER-STORE/src/domain/dtos/product/create-product.dto.ts
@@ -0,0 +1,50 @@
+import { Validators } from "../../../config/validators";
+
+export class CreateProductDto {
+
+    private constructor(
+      public readonly name: string,
+      public readonly available: boolean,
+      public readonly price: number,
+      public readonly description: string,
+      public readonly user: string,
+      public readonly category: string,
+    ) {}
+  
+  
+    static create( object: { [key: string]: any } ):[string?, CreateProductDto?] {
+  
+      const { name, available = false, price, description, user, category } = object;
+      let availableBoolean = available;
+  
+      if ( !name ) return ['Missing name'];
+      if ( typeof available !== 'boolean' ) {
+        availableBoolean = ( available === 'true' )
+      }
+      if ( !user ) return [ 'Missing user' ];
+      if ( !Validators.isMongoID(user) ) return ['Invalid User ID'];
+      
+      if ( !category ) return [ 'Missing category' ];
+      if ( !Validators.isMongoID(category) ) return ['Invalid Category ID'];
+      
+  
+      return [
+        undefined,
+        new CreateProductDto(
+          name,
+          available,
+          price,
+          description,
+          user,
+          category,
+        )
+      ];
+    
+    }
+  
+  
+  }
+  
+  
+  
+  
