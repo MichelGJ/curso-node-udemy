6209906b1a2248397f37a5a9dae04c7ce0b6d58c
michelgj <michjraich12@hotmail.com>
2024-08-20 12:22:07 -0400

    seccion 23

---
+++ 09-github-webhooks/src/presentation/github/controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import { GitHubService, DiscordService } from '../services';
+
+
+
+export class GithubController {
+
+
+  constructor(
+    private readonly githubService = new GitHubService(),
+    private readonly discordService = new DiscordService(),
+  ) { }
+
+
+  webhookHandler = (req: Request, res: Response) => {
+
+    const payload = req.body;
+    const githubEvent = req.header('x-github-event') ?? 'unknown';
+    let message: string;
+
+    switch (githubEvent) {
+
+      case 'star':
+        message = this.githubService.onStar(payload);
+        break;
+
+      case 'issues':
+        message = this.githubService.onIssue(payload);
+        break;
+
+
+      default:
+        message = `Unknown event ${githubEvent}`;
+
+    }
+
+    this.discordService.notify(message)
+      .then( () => res.status(202).send('Accepted') )
+      .catch( () => res.status(500).json({ error: 'internal server error'}) )
+
+  }
+
+
+
+}
+
